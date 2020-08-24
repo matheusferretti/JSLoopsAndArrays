3907c54ad4f67aabd5f15c5bf5acd2128830eac6
Matheus Monteiro <matheusferretti96@gmail.com>
2020-08-24 20:19:11 +0000

    updatedproblemsJS

--- .breathecode/resets/12-Delete-element/app.js
+++ .breathecode/resets/12-Delete-element/app.js
@@ -2,16 +2,7 @@ var people = ['juan','ana','michelle','daniella','stefany','lucy','barak'];
 
 function deletePerson(personName)
 {
-    //your code here
-    let people2 = [ ];
-
-    for(let i = 0; i < people.length; i++){
-        
-    if(people[i] !== personName){
-        people2.push(people[i]);
-    }
-}
-    return people2;
+	//your code here
 }
 
 console.log(deletePerson('daniella'));
