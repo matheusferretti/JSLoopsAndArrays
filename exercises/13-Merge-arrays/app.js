dc6e26a2ef602d05d8eb79b46565fbdabfc69207
Matheus Monteiro <matheusferretti96@gmail.com>
2020-08-24 22:23:54 +0000

    MergeArrays13

--- exercises/13-Merge-arrays/app.js
+++ exercises/13-Merge-arrays/app.js
@@ -1,8 +1,16 @@
 var chunk_one = [ 'Lebron', 'Aaliyah', 'Diamond', 'Dominique', 'Aliyah', 'Jazmin', 'Darnell' ];
 var chunk_two = [ 'Lucas' , 'Jake','Scott','Amy', 'Molly','Hannah','Lucas'];
+let chunk_three = [ ];
 
 const mergeArrays = (firstArray, secondArray) => {
-	//your code here
+    //your code here
+    for(let i = 0; i < firstArray.length; i++){
+        chunk_three.push(firstArray[i]);
+    }
+    for(let b = 0; b < secondArray.length; b++){
+        chunk_three.push(secondArray[b]);
+    }
+    return chunk_three;
 }
 
 console.log(mergeArrays(chunk_one, chunk_two));
