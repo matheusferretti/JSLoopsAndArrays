2cd4bd0b0c73845b8de45a10a65baa5d449c8fc4
Matheus Monteiro <matheusferretti96@gmail.com>
2020-08-26 18:49:52 +0000

    updating

--- exercises/14-Divide-and-Conquer/app.js
+++ exercises/14-Divide-and-Conquer/app.js
@@ -1,5 +1,25 @@
-let list_of_numbers = [4,	80,	85,	59,	37,25,	5,	64,	66,	81,20,	64,	41,	22,	76,76,	55,	96,	2,	68];
+let list_of_numbers = [4, 80, 85,	59,	37,25,	5,	64,	66,	81,20,	64,	41,	22,	76,76,	55,	96,	2,	68];
+let odd = [];
+let even = [];
+let odeven = [];
 
 // your code here
+let mergeTwoList = (list_of_numbers) => {
+    for(let i = 0; i < list_of_numbers.length; i++){
+       
+        if(list_of_numbers[i]%2 === 0)
+        {
+            even.push(list_of_numbers[i]);
+        }
+        else
+        {
+            odd.push(list_of_numbers[i]);
+        }
+    }
+    odeven.push(odd);
+    odeven.push(even);
+    return odeven;
+}
+    
 
-console.log(mergeTwoList(list_of_numbers))
+console.log(mergeTwoList(list_of_numbers).toString());
