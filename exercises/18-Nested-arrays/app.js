22e26ca98601f484a54c4da5277d34ff2ddefe0d
Matheus Monteiro <matheusferretti96@gmail.com>
2020-08-26 20:44:49 +0000

    onfire

--- exercises/18-Nested-arrays/app.js
+++ exercises/18-Nested-arrays/app.js
@@ -1,1 +1,6 @@
 var coordinatesArray = [[33.747252,-112.633853],[-33.867886, -63.987],[41.303921, -81.901693],[-33.350534, -71.653268]];
+
+console.log(coordinatesArray[0][1]);
+console.log(coordinatesArray[1][1]);
+console.log(coordinatesArray[2][1]);
+console.log(coordinatesArray[3][1]);
