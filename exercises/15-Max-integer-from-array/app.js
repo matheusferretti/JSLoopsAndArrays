cde4de0179ab6c3405d6f910bb492309d39fa846
Matheus Monteiro <matheusferretti96@gmail.com>
2020-08-27 00:21:31 +0000

    UpdatedQst15

--- exercises/15-Max-integer-from-array/app.js
+++ exercises/15-Max-integer-from-array/app.js
@@ -1,3 +1,13 @@
 var myArray = [43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34];
+let currMax = myArray[0];
 
-console.log(Math.max(...myArray));
+for(let i = 1; i < myArray.length; i++){
+    if(currMax < myArray[i]){
+        currMax = myArray[i];
+    }else{
+        continue;
+    }
+}
+
+console.log(currMax);
+
