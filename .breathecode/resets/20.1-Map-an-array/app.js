97344af540ca4b52c83522541ae5eaed93447ce6
Matheus Monteiro <matheusferretti96@gmail.com>
2020-08-31 20:54:47 +0000

    updated20

--- .breathecode/resets/20.1-Map-an-array/app.js
+++ .breathecode/resets/20.1-Map-an-array/app.js
@@ -2,10 +2,7 @@ var arrayOfCelsiusValues = [-2,34,56,-10];
 
 var arrayOfFahrenheitValues = arrayOfCelsiusValues.map(function(value){
 	
-    //add your code here and return the new value
-    if(value >= -10){
-       return (value * 9/5) + 32;
-    }
+	//add your code here and return the new value
 	
 });
 
